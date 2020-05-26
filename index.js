5fddf75755d17a6b5df465f1717a896c0a3db09e
Wen-Shiu Hung <catinclay@catinclay-macbookpro.roam.corp.google.com>
2020-05-26 15:19:25 -0700

    some clean up for example

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var imageManager = new ImageManager();
 var soundManager = new SoundManager();
 gameEngine.init(game, imageManager, soundManager, 30);
 
-soundManager.registerSound({name:'failedSound', src:'sounds/failedSound.mp3'});
+soundManager.registerSound({name:'failedSound', src:'sounds/failedSound.mp3'}, /*vol=*/1, /*dup=*/1);
 
 var loadPromises = [
 	imageManager.registerImage({name:'flightImage', path: 'image/', src:'flightIcon.png'}),
